dedd7b8ccecca03889c383a5d9fb353328c72fdf
Alex Lebedev <allebedew@gmail.com>
2018-12-04 16:26:13 +0200

    UI added

---
+++ OktaLogin/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  OktaLogin
+//
+//  Created by Alex on 12/4/18.
+//  Copyright © 2018 Alex. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
---
+++ OktaLogin/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  OktaLogin
+//
+//  Created by Alex on 12/4/18.
+//  Copyright © 2018 Alex. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: - Private
+    
+    @IBOutlet private var loginField: UITextField!
+    @IBOutlet private var passwordField: UITextField!
+    @IBOutlet private var loginButton: UIButton!
+    
+    @IBAction private func loginTapped() {
+        
+    }
+
+}
+
