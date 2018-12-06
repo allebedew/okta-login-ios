f524b130d58f23f59c12edd2fe0a291eadff6ec8
Alex Lebedev <allebedew@gmail.com>
2018-12-06 16:40:52 +0200

    Code exchange request added

--- OktaLogin/OktaLogin.swift
+++ OktaLogin/OktaLogin.swift
@@ -10,35 +10,44 @@ import Foundation
 
 class OktaLogin: NSObject {
     
-    private var session: URLSession!
-    
-    private var state: State { didSet { proceedWithState() } }
-    private var pkce: PKCE
-    
-    private var completion: (() -> Void)?
-    
-    private enum State {
-        case ready(user: String, password: String)
-        case sessionTokenReceived(sessionToken: String)
-        case authCodeReceived(code: String)
+    enum Result {
+        case success(accessToken: String, idToken: String)
         case error(String)
-        case loggedIn
     }
     
-    init(user: String, password: String) {
+    init(baseURL: URL, clientID: String, redirectURI: String, user: String, password: String) {
+        self.baseURL = baseURL
+        self.clientID = clientID
+        self.redirectURI = redirectURI
         state = .ready(user: user, password: password)
-        pkce = PKCE()
         super.init()
         session = URLSession(configuration: URLSessionConfiguration.default, delegate: self, delegateQueue: nil)
     }
     
-    func login(completion: @escaping () -> Void) {
+    func login(completion: @escaping (Result) -> Void) {
         guard case .ready(_, _) = state else { return }
         self.completion = completion
         proceedWithState()
     }
     
-    func proceedWithState() {
+    // MARK: - Private
+    
+    private var baseURL: URL
+    private var clientID: String
+    private var redirectURI: String
+    private var state: State { didSet { proceedWithState() } }
+    private var session: URLSession!
+    private var completion: ((Result) -> Void)?
+    
+    private enum State {
+        case ready(user: String, password: String)
+        case sessionTokenReceived(sessionToken: String)
+        case authCodeReceived(code: String, pkce: PKCE)
+        case error(String)
+        case loggedIn(accessToken: String, idToken: String)
+    }
+    
+    private func proceedWithState() {
         switch state {
         case .ready(user: let user, password: let password):
             print("Performing primary authenication for \(user) ...")
@@ -47,40 +56,48 @@ class OktaLogin: NSObject {
             print("Session Token received: \(token)")
             print("Performing authorization code request ...")
             makeAuthorizationCodeRequest(sessionToken: token)
-        case .authCodeReceived(code: let code):
+        case .authCodeReceived(code: let code, pkce: let pkce):
             print("Authenication code received: \(code)")
-            
-            
-            
-            
-            
-            
-        default:
-            break
+            print("Performing code exchange request ...")
+            makeCodeExchangeRequest(code: code, pkce: pkce)
+        case .loggedIn(accessToken: let accessToken, idToken: let idToken):
+            print("*** LOGGED IN ***")
+            DispatchQueue.main.async {
+                self.completion?(.success(accessToken: accessToken, idToken: idToken))
+            }
+        case .error(let error):
+            print("ERROR: \(error)")
+            DispatchQueue.main.async {
+                self.completion?(.error(error))
+            }
         }
     }
     
-    func makePrimaryAuthenicationRequest(user: String, password: String) {
-        let url = URL(string: "https://lohika-um.oktapreview.com/api/v1/authn")!
+    private func makePrimaryAuthenicationRequest(user: String, password: String) {
+        
+        var url = URLComponents(url: baseURL, resolvingAgainstBaseURL: false)!
+        url.path = "/api/v1/authn"
+
         let body: [String: Any] = ["username": user,
                                    "password": password,
                                    "relayState": "test",
                                    "options": ["multiOptionalFactorEnroll": false,
                                                "warnBeforePasswordExpired": false]]
-        var request = URLRequest(url: url)
+        var request = URLRequest(url: url.url!)
         request.httpMethod = "POST"
         request.httpBody = try? JSONSerialization.data(withJSONObject: body, options: [])
         request.setValue("application/json", forHTTPHeaderField: "Accept")
         request.setValue("application/json", forHTTPHeaderField: "Content-Type")
         
         let task = session.dataTask(with: request) { data, response, error in
-            guard let statusCode = (response as? HTTPURLResponse)?.statusCode else {
-                self.state = .error("No response")
+            guard error == nil else {
+                self.state = .error(error!.localizedDescription)
                 return
             }
-            guard statusCode == 200 else {
+            let response = response as! HTTPURLResponse
+            guard response.statusCode == 200 else {
                 let body = String(data: data ?? Data(), encoding: .utf8)
-                self.state = .error("Response: [\(statusCode)]\n\(body ?? "?")")
+                self.state = .error("Response: [\(response.statusCode)]\n\(body ?? "?")")
                 return
             }
             guard let data = data,
@@ -97,26 +114,29 @@ class OktaLogin: NSObject {
         task.resume()
     }
     
-    func makeAuthorizationCodeRequest(sessionToken: String) {
-        let baseURL = "https://lohika-um.oktapreview.com"
-        let params: [String: String] = ["client_id": "0oahng9nv4VQeNpvn0h7",
+    private func makeAuthorizationCodeRequest(sessionToken: String) {
+        
+        let pkce = PKCE()
+        
+        let params: [String: String] = ["client_id": clientID,
                                         "response_type": "code",
                                         "scope": "openid offline_access",
-                                        "redirect_uri": "com.ios-okta-sdk-redirect-test-app:/callback",
+                                        "redirect_uri": redirectURI,
                                         "state": String.randomAlphanumeric(length: 64),
                                         "code_challenge_method": "S256",
                                         "code_challenge": pkce.codeChallange,
                                         "sessionToken": sessionToken]
         
-        var url = URLComponents(string: baseURL)!
+        var url = URLComponents(url: baseURL, resolvingAgainstBaseURL: false)!
         url.path = "/oauth2/default/v1/authorize"
         url.queryItems = params.map { URLQueryItem(name: $0.key, value: $0.value) }
         
         let task = session.dataTask(with: url.url!) { data, response, error in
-            guard let response = response as? HTTPURLResponse else {
-                self.state = .error("No response")
+            guard error == nil else {
+                self.state = .error(error!.localizedDescription)
                 return
             }
+            let response = response as! HTTPURLResponse
             guard response.statusCode == 302 else {
                 self.state = .error("Unexpected status code (\(response.statusCode))")
                 return
@@ -129,10 +149,55 @@ class OktaLogin: NSObject {
                     return
                     
             }
-            self.state = .authCodeReceived(code: code)
+            self.state = .authCodeReceived(code: code, pkce: pkce)
         }
         task.resume()
     }
+    
+    private func makeCodeExchangeRequest(code: String, pkce: PKCE) {
+        
+        let params: [String: String] = ["grant_type": "authorization_code",
+                                        "client_id": clientID,
+                                        "redirect_uri": redirectURI,
+                                        "code": code,
+                                        "code_verifier": pkce.codeVerifier]
+        
+        var url = URLComponents(url: baseURL, resolvingAgainstBaseURL: false)!
+        url.path = "/oauth2/default/v1/token"
+        
+        var request = URLRequest(url: url.url!)
+        request.httpMethod = "POST"
+        request.httpBody = params.map({ "\($0.key)=\($0.value)" }).joined(separator: "&").data(using: .utf8)
+        request.setValue("application/json", forHTTPHeaderField: "Accept")
+        request.setValue("no-cache", forHTTPHeaderField: "Cache-Control")
+        request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
+        request.httpShouldHandleCookies = false
+        
+        let task = session.dataTask(with: request) { data, response, error in
+            guard error == nil else {
+                self.state = .error(error!.localizedDescription)
+                return
+            }
+            let response = response as! HTTPURLResponse
+            guard response.statusCode == 200 else {
+                self.state = .error("Unexpected status code (\(response.statusCode))")
+                return
+            }
+            guard let data = data,
+                let json = try? JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] else {
+                    self.state = .error("Error parsing server response")
+                    return
+            }
+            guard let accessToken = json?["access_token"] as? String,
+                let idToken = json?["id_token"] as? String else {
+                self.state = .error("Tokens not found")
+                return
+            }
+            self.state = .loggedIn(accessToken: accessToken, idToken: idToken)
+        }
+        task.resume()
+    }
+    
 }
 
 extension OktaLogin: URLSessionTaskDelegate {
--- OktaLogin/ViewController.swift
+++ OktaLogin/ViewController.swift
@@ -9,12 +9,6 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-    
-    // MARK: - Private
     
     @IBOutlet private var loginField: UITextField!
     @IBOutlet private var passwordField: UITextField!
@@ -26,11 +20,31 @@ class ViewController: UIViewController {
         guard let user = loginField.text, let password = passwordField.text else { return }
         
         activityIndicator.startAnimating()
+        loginButton.isEnabled = false
         
-        OktaAPI.login(user: user, password: password) { [weak self] in
-            
+        OktaLogin(baseURL: URL(string: "https://myorg.okta.com")!,
+                  clientID: "", // your application client_id
+                  redirectURI: "", // your application login redirect url, e.g. com.okta.myapp:/callback
+                  user: user,
+                  password: password).login() { [weak self] result in
+                    
             self?.activityIndicator.stopAnimating()
+            self?.loginButton.isEnabled = true
+            
+            switch result {
+            case .success(accessToken: let accessToken, idToken: let idToken):
+                let alert = UIAlertController(title: "Success!",
+                                              message: "Access Token:\n\(accessToken)\n\nID Token:\n\(idToken)",
+                                              preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                self?.present(alert, animated: true, completion: nil)
+            case .error(let error):
+                let alert = UIAlertController(title: "Error :-(",
+                                              message: error,
+                                              preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                self?.present(alert, animated: true, completion: nil)
+            }
         }
     }
 }
-
